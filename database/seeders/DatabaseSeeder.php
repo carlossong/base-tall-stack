a48efc7804baa4dcd51d2cd005d45ad45d5c1227
José Carlos <carlossong@gmail.com>
2023-05-09 17:25:39 -0300

    Seeder clear

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,14 +14,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        \App\Models\User::factory()->create([
+        User::factory()->create([
             'name' => 'José Carlos',
             'email' => 'admin@admin.com',
             'password' => bcrypt('123Mudar'),
             'email_verified_at' => now(),
         ]);
-
-        \App\Models\User::factory(10)->create();
 
         $this->call([
             PermissionSeeder::class,
