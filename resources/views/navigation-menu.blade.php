0d507e588cdc4376477c1dcfeb9f96c93c67e212
José Carlos <carlossong@gmail.com>
2023-05-09 17:38:38 -0300

    navigation gate view

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -17,9 +17,11 @@
                         {{ __('Dashboard') }}
                     </x-nav-link>
 
-                    <x-nav-link href="{{ route('user.index') }}" :active="request()->routeIs('user.*')">
-                        {{ __('Usuários') }}
-                    </x-nav-link>
+                    @can('user_access')
+                        <x-nav-link href="{{ route('user.index') }}" :active="request()->routeIs('user.*')">
+                            {{ __('Usuários') }}
+                        </x-nav-link>
+                    @endcan
                 </div>
             </div>
 
@@ -79,14 +81,16 @@
                 <!-- Settings Dropdown -->
                 <div class="relative">
                     <button x-cloak x-on:click="darkMode = !darkMode;">
-                        <svg x-show="darkMode" class="p-2 w-8 h-8 text-gray-100 bg-gray-700 rounded-full transition cursor-pointer dark:hover:bg-gray-600"
+                        <svg x-show="darkMode"
+                            class="p-2 w-8 h-8 text-gray-100 bg-gray-700 rounded-full transition cursor-pointer dark:hover:bg-gray-600"
                             xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                             stroke="currentColor">
                             <path stroke-linecap="round" stroke-linejoin="round"
                                 d="M12 3v2.25m6.364.386l-1.591 1.591M21 12h-2.25m-.386 6.364l-1.591-1.591M12 18.75V21m-4.773-4.227l-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z" />
                         </svg>
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                            stroke="currentColor" x-show="!darkMode" class="p-2 w-8 h-8 text-gray-700 bg-gray-100 rounded-full transition cursor-pointer hover:bg-gray-200">
+                            stroke="currentColor" x-show="!darkMode"
+                            class="p-2 w-8 h-8 text-gray-700 bg-gray-100 rounded-full transition cursor-pointer hover:bg-gray-200">
                             <path stroke-linecap="round" stroke-linejoin="round"
                                 d="M21.752 15.002A9.718 9.718 0 0118 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 003 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 009.002-5.998z" />
                         </svg>
@@ -150,14 +154,16 @@
             <!-- Hamburger -->
             <div class="-mr-2 flex items-center sm:hidden">
                 <button x-cloak x-on:click="darkMode = !darkMode;">
-                    <svg x-show="darkMode" class="w-8 h-8 text-gray-100 bg-gray-700 rounded-md transition cursor-pointer dark:hover:bg-gray-600"
+                    <svg x-show="darkMode"
+                        class="w-8 h-8 text-gray-100 bg-gray-700 rounded-md transition cursor-pointer dark:hover:bg-gray-600"
                         xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                         stroke="currentColor">
                         <path stroke-linecap="round" stroke-linejoin="round"
                             d="M12 3v2.25m6.364.386l-1.591 1.591M21 12h-2.25m-.386 6.364l-1.591-1.591M12 18.75V21m-4.773-4.227l-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z" />
                     </svg>
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                        stroke="currentColor" x-show="!darkMode" class="w-8 h-8 text-gray-700 bg-gray-100 rounded-md transition cursor-pointer hover:bg-gray-200">
+                        stroke="currentColor" x-show="!darkMode"
+                        class="w-8 h-8 text-gray-700 bg-gray-100 rounded-md transition cursor-pointer hover:bg-gray-200">
                         <path stroke-linecap="round" stroke-linejoin="round"
                             d="M21.752 15.002A9.718 9.718 0 0118 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 003 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 009.002-5.998z" />
                     </svg>
